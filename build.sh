ff7c45b0edd01a4470c4ad542f0d4da1991686aa
SHADOW <unsterblich.wang@gmail.com>
2024-09-14 00:21:17 +0800

    一些自己的修改，为了让代码提示正常

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 set -x
 
 SOURCE_DIR=`pwd`
-BUILD_DIR=${BUILD_DIR:-../build}
+BUILD_DIR=${BUILD_DIR:-./build}
 BUILD_TYPE=${BUILD_TYPE:-release}
-INSTALL_DIR=${INSTALL_DIR:-../${BUILD_TYPE}-install-cpp17}
+INSTALL_DIR=${INSTALL_DIR:-./${BUILD_TYPE}-install-cpp17}
 CXX=${CXX:-g++}
 
 ln -sf $BUILD_DIR/$BUILD_TYPE-cpp17/compile_commands.json
